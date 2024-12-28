ddaaa7bd3324cb38dbbfab5a0376377b085d42d6
Sanket <sanket.ingale1998@gmail.com>
2024-12-29 00:20:16 +0530

    fix: updated links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,12 @@ export default function Footer() {
       <img src={require('../images/hexagons.png')} alt='hex'/>
       <div className='footer--right'>
         <div className='name'>Sanket Ingale</div>
-        <span>+91 9404 26 4995 | Pune (MH), India</span>
+        <span><a className='tel' href='tel:9404264995'>+91 9404 26 4995</a> | Pune (MH), India</span>
         <div className='links'>
-          <a target='blank' href='https://www.linkedin.com/in/sanket-ingale1998/'>LinkedIn</a>
-          <a target='blank' href='https://github.com/sanket-ingale'>Github</a>
-          <a target='blank' href='https://www.instagram.com/lightbuster_/'>Instagram</a>
-          <a target='blank' href='mailto:sanket.ingale1998@gmail.com'>Mail me</a>
+          <a target='_blank' href='https://www.linkedin.com/in/sanket-ingale1998/'>LinkedIn</a>
+          <a target='_blank' href='https://github.com/sanket-ingale'>Github</a>
+          <a target='_blank' href='https://www.instagram.com/lightbuster_/'>Instagram</a>
+          <a target='_top' href='mailto:sanket.ingale1998@gmail.com'>Mail me</a>
         </div>
       </div>
     </div>
